78b6660c6c9a5d8b819290c88629279ce83a9c7d
Philip Tallo <tallopt@mail.uc.edu>
2017-10-21 22:18:03 -0400

    initializing file structure
    added require to index.js and made it such that index.html now links to external style sheets in js sheet.

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+  app.use(express.static(__dirname + '/public'));
+
+});
+
+io.on("connection", function(){
+
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
---
+++ public/main.js
@@ -0,0 +1,8 @@
+
+$('li').on('mouseenter', function(){
+  $('li').css('background-color', 'grey');
+});
+
+$('li').on('mouseleave', function(){
+    $('li').css('background-color', 'lightblue');
+});
