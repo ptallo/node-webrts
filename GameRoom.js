767bedcd761984d424ab4c63d35e3663db0a3bb3
Philip Tallo <tallopt@mail.uc.edu>
2017-10-24 23:54:27 -0400

    created initial game room info
    created game room class, as well as atempted to add game room object upon button push. Before game rooms can be fully implemented, user accounts will have to be created.

---
+++ GameRoom.js
@@ -0,0 +1,28 @@
+class GameRoom{
+  constructor(){
+    this.num_players = 2;
+    this.players = [];
+    var date = new Date;
+    this.game_id = date.getTime();
+  }
+  add_player(pid){
+    if (this.players.length <= 2){
+      this.players.push(pid);
+    } else {
+      throw "There are too many players in this game room!";
+    }
+  }
+  remove_player(pid){
+    var indexToRemove = null;
+    for(var i = 0; i < this.players.length; i++){
+      if (this.players[i] == pid) {
+        indexToRemove = i;
+      }
+    }
+    if(indexToRemove != null){
+      this.players.splice(indexToRemove,1);
+    } else {
+      throw "That player is no longer in this lobby!";
+    }
+  }
+}
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express = require('express')
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var {GameRoom} = require("./GameRoom.js");
 var port = 8080;
+var game_rooms = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -11,7 +13,9 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('add lobby', function(){
-    console.log('add game room');
+    io.emit('add lobby');
+    var game = new GameRoom();
+    game_rooms.push(game);
   });
 });
 
