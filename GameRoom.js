e006b184aca64d1cf336cc9e6ef4e9db40308eae
Philip Tallo <tallopt@mail.uc.edu>
2017-10-28 14:07:10 -0400

    fixed indentation

--- GameRoom.js
+++ GameRoom.js
@@ -3,30 +3,30 @@ var shortid = require('shortid');
 
 class GameRoom{
   constructor(name){
+    this.path = "/game_room";
     this.name = name;
-    this.num_players = 2;
     this.players = [];
-    var date = new Date;
+    this.num_players = 2;
     this.game_id = shortid.generate();
   }
   add_player(pid){
     if (this.players.length <= 2){
-      this.players.push(pid);
+        this.players.push(pid);
     } else {
-      throw "There are too many players in this game room!";
+        throw "There are too many players in this game room!";
     }
   }
   remove_player(pid){
-    var indexToRemove = null;
-    for(var i = 0; i < this.players.length; i++){
-      if (this.players[i] == pid) {
-        indexToRemove = i;
-      }
+    let indexToRemove = null;
+    for(let i = 0; i < this.players.length; i++){
+        if (this.players[i] == pid) {
+          indexToRemove = i;
+        }
     }
     if(indexToRemove != null){
-      this.players.splice(indexToRemove,1);
+        this.players.splice(indexToRemove,1);
     } else {
-      throw "That player is no longer in this lobby!";
+        throw "That player is no longer in this lobby!";
     }
   }
 }
--- index.js
+++ index.js
@@ -11,28 +11,28 @@ var port = 8080;
 var game_rooms = [];
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-  app.use(express.static(__dirname + '/public'));
+    res.sendFile(__dirname + '/index.html');
+    app.use(express.static(__dirname + '/public'));
 });
 
 io.on('connection', function(socket){
-  addGameRooms(socket);
-  socket.on('add lobby', function(){
-    var game = new GameRoom("test");
-    game_rooms.push(game);
-    socket.emit('add room', JSON.stringify(game));
-  });
-  socket.on('refresh', function(){
     addGameRooms(socket);
-  })
+    socket.on('add lobby', function(){
+        let game = new GameRoom("test");
+        game_rooms.push(game);
+        socket.emit('add room', JSON.stringify(game));
+    });
+    socket.on('refresh', function(){
+        addGameRooms(socket);
+    })
 });
 
 http.listen(port, function(){
-  console.log('listening on *: ' + port);
+    console.log('listening on *: ' + port);
 });
 
 function addGameRooms(socket){
-  for(var i = 0; i < game_rooms.length; i++){
-    socket.emit('add room', JSON.stringify(game_rooms[i]));
-  }
+    for(let i = 0; i < game_rooms.length; i++){
+        socket.emit('add room', JSON.stringify(game_rooms[i]));
+    }
 }
