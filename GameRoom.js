4fb31910ef83fd83a2d63b0b213c2f5871930ae8
Philip Tallo <tallopt@mail.uc.edu>
2017-11-15 21:43:34 -0500

    reformatted due to indentation

--- GameRoom.js
+++ GameRoom.js
@@ -2,22 +2,24 @@
 var shortid = require('shortid');
 
 class GameRoom{
-  constructor(name){
-    this.name = name;
-    this.players = [];
-    this.num_players = 2;
-    this.game_id = shortid.generate();
-  }
-  add_player(pid){
-    if (this.players.length < this.num_players){
-        this.players.push(pid);
-    } else {
-        throw "There are too many players in this game room!";
+    constructor(name){
+        this.name = name;
+        this.players = [];
+        this.num_players = 2;
+        this.game_id = shortid.generate();
     }
-  }
-    remove_player(pid){
+    add_player(player_json){
+        let player = JSON.parse(player_json);
+        if (this.players.length < this.num_players){
+            this.players.push(player);
+        } else {
+            throw "There are too many players in this game room!";
+        }
+    }
+    remove_player(player_json){
+        let player = JSON.parse(player_json);
         for(let i = 0; i < this.players.length; i++){
-            if (this.players[i] == pid) {
+            if (this.players[i].id == player.ID) {
                 try {
                     this.players.splice(i, 1);
                 } catch (e) {
