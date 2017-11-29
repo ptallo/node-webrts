9d5dd8ca63084c3695c6c158fcf5538b3a336842
Philip Tallo <tallopt@mail.uc.edu>
2017-11-28 23:44:28 -0500

    radical code redesign
    eliminated uneeded code and reorganized so that it will be more readable.

---
+++ server_js/GameRoom.js
@@ -0,0 +1,22 @@
+'use strict';
+var shortid = require('shortid');
+
+class GameRoom{
+    constructor(name){
+        this.name = name;
+        this.players = [];
+        this.id = shortid.generate();
+    }
+    addPlayer(player){
+        this.players.push(player);
+    }
+    removePlayer(player){
+        for(let i = 0; i < this.players.length; i++){
+            if (this.players[i].id == player.id){
+                this.players.splice(i,1);
+            }
+        }
+    }
+}
+
+module.exports = GameRoom;
