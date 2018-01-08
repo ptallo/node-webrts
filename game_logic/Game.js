44a7cd2bb01ec60e87aa07d65b99a22406e65601
Philip Tallo <tallopt@mail.uc.edu>
2018-01-08 12:52:59 -0500

    created a drawGame function and added canvas vars to game.js

--- game_logic/Game.js
+++ game_logic/Game.js
@@ -7,6 +7,7 @@ class Game{
         this.id = id == "none" ? shortid.generate() : id;
         this.gameObjects = [];
         this.gameObjects.push(new GameObject(20, 20, 40, 40));
+        this.gameObjects.push(new GameObject(80, 80, 100, 20))
     }
     update(){
         for(let object in this.gameObjects){
--- public/js/game.js
+++ public/js/game.js
@@ -1,13 +1,37 @@
 'use strict';
-// browserify main.js -o bundle.js
+// browserify main.js -o bundle.js - game logic require
 var socket = io();
 var Game = require("../../game_logic/Game.js");
 var GameObject = require('../../game_logic/GameObject.js');
 var PositionComponent = require('../../game_logic/component/PositionComponent.js');
 var SizeComponent = require('../../game_logic/component/SizeComponent.js');
+var VelocityComponent = require('../../game_logic/component/VelocityComponent.js');
+
+//Other global variables which need to be expressed
+var canvas = document.getElementById("game_canvas");
+var ctx = canvas.getContext("2d");
+let game_id = sessionStorage.getItem('game_id');
+var game = new Game(game_id);
+
 
 $(document).ready(function () {
-    let game_id = sessionStorage.getItem('game_id');
     socket.emit('join io room', game_id);
-    var game = new Game(game_id);
+    try {
+        drawGame(game);
+    } catch (e) {
+        $('p').text(e);
+    }
 });
+
+function drawGame() {
+    ctx.fillStyle = "#38cceb";
+    for (let i = 0; i < game.gameObjects.length; i++) {
+        let gameObject = game.gameObjects[i];
+        let x = gameObject.positionComponent.x;
+        let y = gameObject.positionComponent.y;
+        let width = gameObject.sizeComponent.width;
+        let height = gameObject.sizeComponent.height;
+        $('p').text(x + ", " + y + ", " + width + ", " + height);
+        ctx.fillRect(x, y, width, height);
+    }
+}
