be2c7e3eab3467c2c33b74777185c354c7453088
Philip Tallo <tallopt@mail.uc.edu>
2018-04-11 23:25:32 -0400

    refactored Utility and added collision check for unknown object types

--- server/Utility.js
+++ server/Utility.js
@@ -4,7 +4,7 @@ class Utility {
         return rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x &&
             rect1.y < rect2.y + rect2.height && rect1.height + rect1.y > rect2.y;
     }
-    static checkCircleRectCollision(rect, circle){
+    static checkRectCircleCollision(rect, circle){
         let DeltaX = circle.x - Math.max(rect.x, Math.min(circle.x, rect.x + rect.width));
         let DeltaY = circle.y - Math.max(rect.y, Math.min(circle.y, rect.y + rect.height));
         return (DeltaX * DeltaX + DeltaY * DeltaY) < (circle.radius * circle.radius);
@@ -14,6 +14,17 @@ class Utility {
         let dy = circle1.y - circle2.y;
         let distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
         return distance < circle1.radius + circle2.radius;
+    }
+    static checkUnknownObjectCollision(object1, object2){
+        if (object1.type === "RectPhysicsComponent" && object2.type === "RectPhysicsComponent"){
+            return Utility.checkRectRectCollision(object1.rect, object2.rect);
+        } else if (object1.type ==="RectPhysicsComponent" && object2.type === "CirclePhysicsComponent"){
+            return Utility.checkRectCircleCollision(object1.rect, object2.circle);
+        } else if (object1.type ==="CirclePhysicsComponent" && object2.type === "RectPhysicsComponent"){
+            return Utility.checkRectCircleCollision(object2.rect, object1.circle);
+        } else if (object1.type ==="CirclePhysicsComponent" && object2.type === "CirclePhysicsComponent"){
+            return Utility.checkCircleCircleCollision(object1.circle, object2.circle);
+        }
     }
 }
 
